a5225a17e76e2bd5d40a81952dff759986b0e4b9
Evgeniy Titov <titevg@gmail.com>
2026-04-11 23:25:20 +0300

    edit create scripts

--- create_music_shop_db.sql
+++ create_music_shop_db.sql
@@ -1,29 +1,29 @@
 CREATE TABLE IF NOT EXISTS Genres (
-	id int PRIMARY KEY,
+	genres_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	title varchar(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Musician (
-	id int PRIMARY KEY,
+	musician_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	name varchar(100) NOT NULL 
 );
 
 CREATE TABLE IF NOT EXISTS Albums (
-	id int PRIMARY KEY,
+	albums_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	title varchar(100) NOT NULL,
 	year SMALLINT NOT NULL,
 	CONSTRAINT year CHECK (year >= 1900  AND  year <= 2100)
 );
 
 CREATE TABLE IF NOT EXISTS Collections (
-	id int PRIMARY KEY,
+	collections_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	title varchar(100) NOT NULL,
 	year SMALLINT NOT NULL,
 	CONSTRAINT date CHECK (year >= 1900  AND  year <= 2100)
 );
 
 CREATE TABLE IF NOT EXISTS Genres_Musician (
-	id int PRIMARY KEY,
+	genres_musician_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	genres_id int,
 	musician_id int,
 	FOREIGN KEY (genres_id) REFERENCES Genres(id),
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS Genres_Musician (
 );
 
 CREATE TABLE IF NOT EXISTS Musician_Albums (
-	id int PRIMARY KEY,
+	musician_albums_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	musician_id int,
 	albums_id int,
 	FOREIGN KEY (musician_id) REFERENCES Musician(id),
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS Musician_Albums (
 );
 
 CREATE TABLE IF NOT EXISTS Tracks (
-	id int PRIMARY KEY,
+	tracks_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	albums_id int,
 	name varchar(100) NOT NULL,
 	duration_sec int NOT NULL,
@@ -47,14 +47,10 @@ CREATE TABLE IF NOT EXISTS Tracks (
 );
 
 CREATE TABLE IF NOT EXISTS Collection_Tracks (
-	id int PRIMARY KEY,
+	collections_tracks_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	collections_id int,
 	tracks_id int,
 	FOREIGN KEY (collections_id) REFERENCES Collections(id),
 	FOREIGN KEY (tracks_id) REFERENCES Tracks(id)
 );
-
-
-
-
 
