6b2949b1f3a2372762304e7c62408654a3fe6692
Evgeniy Titov <titevg@gmail.com>
2026-04-12 12:46:17 +0300

    correct id'ies on creating tables

--- create_music_shop_db.sql
+++ create_music_shop_db.sql
@@ -26,16 +26,16 @@ CREATE TABLE IF NOT EXISTS Genres_Musician (
 	genres_musician_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	genres_id int,
 	musician_id int,
-	FOREIGN KEY (genres_id) REFERENCES Genres(id),
-	FOREIGN KEY (musician_id) REFERENCES Musician(id)
+	FOREIGN KEY (genres_id) REFERENCES Genres(genres_id),
+	FOREIGN KEY (musician_id) REFERENCES Musician(musician_id)
 );
 
 CREATE TABLE IF NOT EXISTS Musician_Albums (
 	musician_albums_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	musician_id int,
 	albums_id int,
-	FOREIGN KEY (musician_id) REFERENCES Musician(id),
-	FOREIGN KEY (albums_id) REFERENCES Albums(id)
+	FOREIGN KEY (musician_id) REFERENCES Musician(musician_id),
+	FOREIGN KEY (albums_id) REFERENCES Albums(albums_id)
 );
 
 CREATE TABLE IF NOT EXISTS Tracks (
@@ -43,14 +43,14 @@ CREATE TABLE IF NOT EXISTS Tracks (
 	albums_id int,
 	name varchar(100) NOT NULL,
 	duration_sec int NOT NULL,
-	FOREIGN KEY (albums_id) REFERENCES Albums(id)
+	FOREIGN KEY (albums_id) REFERENCES Albums(albums_id)
 );
 
 CREATE TABLE IF NOT EXISTS Collection_Tracks (
 	collections_tracks_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	collections_id int,
 	tracks_id int,
-	FOREIGN KEY (collections_id) REFERENCES Collections(id),
-	FOREIGN KEY (tracks_id) REFERENCES Tracks(id)
+	FOREIGN KEY (collections_id) REFERENCES Collections(collections_id),
+	FOREIGN KEY (tracks_id) REFERENCES Tracks(tracks_id)
 );
 
